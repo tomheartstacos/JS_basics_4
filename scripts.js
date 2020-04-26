f465243bed2ad639cbf479c9b5490f13e514a3d6
Tom Viemont <46653205+tomheartstacos@users.noreply.github.com>
2020-04-26 14:49:26 +0100

    Initial upload of three files

---
+++ scripts.js
@@ -0,0 +1,21 @@
+var repositoryList = [
+  {
+    creatureName : 'Metapod',
+    creatureHeight : 0.7,
+    creatureAbilities : ['Shed-skin'],
+    creatureTypes : ['Bug'],
+  },
+  {
+    creatureName : 'Charmander',
+    creatureHeight : 2.00,
+    creatureAbilities : ['Blaze', 'Solar-power'],
+    creatureTypes : ['Fire'],
+  },
+  {
+    creatureName : 'Squirtle',
+    creatureHeight : 0.5,
+    creatureAbilities : ['Rain-dish', 'Torrent'],
+    creatureTypes : ['Monster', 'Water  1'],
+  },
+];
+repositoryList.forEach(element => console.log(element));
